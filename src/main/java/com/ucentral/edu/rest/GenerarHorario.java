76271303df8a36717f04cd0c62418c6221a2de01
Deida19 <69435112+Deida19@users.noreply.github.com>
2021-05-09 10:54:30 -0500

    09052021

--- src/main/java/com/ucentral/edu/rest/GenerarHorario.java
+++ src/main/java/com/ucentral/edu/rest/GenerarHorario.java
@@ -7,6 +7,7 @@ import java.util.OptionalInt;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -50,7 +51,7 @@ public class GenerarHorario {
 	@Autowired
 	private HorarioXEstudianteServicio horarioXEstudianteServicio;
 	
-	
+	@CrossOrigin
 	@RequestMapping(value = "login", method = RequestMethod.GET)
 	public ResponseEntity<Estudiante> autenticacion(@RequestParam ("usuario") String usuario, @RequestParam("pass") String pass){
 		
